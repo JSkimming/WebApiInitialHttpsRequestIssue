b8f97e71a62b7b23b6bc10394fa675abd741791b
James Skimming <gitcommits@skimming.net>
2013-02-27 17:11:49 +0000

    Added test controllers

---
+++ WebApiInitialHttpsRequestIssue/App_Start/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace WebApiInitialHttpsRequestIssue.App_Start
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return Redirect("~/api/test");
+        }
+    }
+}
