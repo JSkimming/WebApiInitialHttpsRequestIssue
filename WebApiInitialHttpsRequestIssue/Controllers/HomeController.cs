ee126b30962e8219bac15e69cdb49c2435f8bf2f
James Skimming <gitcommits@skimming.net>
2013-03-20 10:33:23 +0000

    Added client API tests (calling itself)

--- WebApiInitialHttpsRequestIssue/Controllers/HomeController.cs
+++ WebApiInitialHttpsRequestIssue/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace WebApiInitialHttpsRequestIssue.Controllers
 {
@@ -7,11 +10,49 @@ namespace WebApiInitialHttpsRequestIssue.Controllers
         //
         // GET: /Home/
 
+        private static HttpClient GetClient(Uri requestUrl)
+        {
+            var baseUri =
+                requestUrl.AbsoluteUri.Substring(0,
+                                                 requestUrl.AbsoluteUri.LastIndexOf(requestUrl.PathAndQuery,
+                                                                                    StringComparison.Ordinal));
+            return new HttpClient(new TraceHttpClientHandler())
+                {
+                    BaseAddress = new Uri(baseUri),
+                };
+        }
+
         public ActionResult Index()
         {
             MvcApplication.LogFormat("Redirecting from '{0}'.", Request.Url);
 
             return RedirectPermanent("~/api/test");
+        }
+
+        public async Task<ActionResult> TestGet()
+        {
+            using (var client = GetClient(Request.Url))
+            {
+                HttpResponseMessage responseMessage = await client.GetAsync("api/test");
+
+                string result = await responseMessage.Content.ReadAsStringAsync();
+
+                return View((object)result);
+            }
+        }
+
+        public async Task<ActionResult> TestPost()
+        {
+            using (var client = GetClient(Request.Url))
+            {
+                var content = new StringContent(string.Format("Here is some test data '{0}'.", Guid.NewGuid()));
+
+                HttpResponseMessage responseMessage = await client.PostAsync("api/test", content);
+
+                string result = await responseMessage.Content.ReadAsStringAsync();
+
+                return View((object) result);
+            }
         }
     }
 }
---
+++ WebApiInitialHttpsRequestIssue/TraceHttpClientHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApiInitialHttpsRequestIssue
+{
+    public class TraceHttpClientHandler : HttpClientHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            MvcApplication.LogFormat("{0} {1}", request.Method.Method, request.RequestUri);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
